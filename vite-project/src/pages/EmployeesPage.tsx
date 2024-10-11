411a0eec0bd1e965da58fa4e7aa6270466ff63fa
gq <guanquntan@gmail.com>
2024-10-11 16:20:28 +0800

    wip toggle

--- vite-project/src/pages/EmployeesPage.tsx
+++ vite-project/src/pages/EmployeesPage.tsx
@@ -12,19 +12,20 @@ type EmployeeInfo = {
 
 type IconToggleProps = {
     enabled: boolean,
-    icon: string
+    icon: string,
+    onToggle: () => void
 };
-const IconToggle: React.FC<IconToggleProps> = ({ enabled, icon }: IconToggleProps) => {
+const IconToggle: React.FC<IconToggleProps> = ({ enabled, icon, onToggle }: IconToggleProps) => {
     return (
         <div className="flex">
             <label className="inline-flexflex relative items-center cursor-pointer">
                 {enabled &&
-                    <input type="checkbox" value="" checked className="sr-only peer" onClick={() => enabled = !enabled} />
+                    <input type="checkbox" value="" checked className="sr-only peer" onClick={onToggle} />
 
                 }
                 {!enabled &&
                     <input type="checkbox" value="" className="sr-only peer"
-                        onClick={() => enabled = !enabled} />
+                        onClick={onToggle} />
 
                 }
                 <div className="w-9 h-5 bg-gray-200 hover:bg-gray-300 peer-focus:outline-0 peer-focus:ring-transparent rounded-full peer transition-all ease-in-out duration-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-indigo-600 hover:peer-checked:bg-indigo-700"></div>
@@ -36,17 +37,28 @@ const IconToggle: React.FC<IconToggleProps> = ({ enabled, icon }: IconToggleProp
     );
 }
 
-const EmployeeCard: React.FC<EmployeeInfo> = ({ name, textToSpeechEnabled, enlargedText }: EmployeeInfo) => {
+type EmployeeCardProps = EmployeeInfo & {
+    onToggleTextToSpeech: () => void,
+    onToggleEnlargeText: () => void
+
+}
+
+const EmployeeCard: React.FC<EmployeeCardProps> = ({ name, textToSpeechEnabled, enlargedText, onToggleEnlargeText, onToggleTextToSpeech }: EmployeeCardProps) => {
     return (
-        <div className='flex flex-row w-7/8 h-full p-2 m-1 border rounded-md justify-center'>
-            <p className='text-left basis-3/5 text-slate-800'>{name}</p>
-            <div className='basis-1/5'>
+        <div className='flex flex-row w-7/8 h-full p-2 m-1 border rounded-md justify-center items-center '>
+            <p className='text-left basis-3/6 text-slate-800'>{name}</p>
+            <div className='basis-1/6'>
                 <IconToggle enabled={textToSpeechEnabled}
-                    icon={SpeakerIcon}></IconToggle>
+                    icon={SpeakerIcon}
+                    onToggle={onToggleTextToSpeech}></IconToggle>
             </div>
-            <div className='basis-1/5'>
+            <div className='basis-1/6'>
                 <IconToggle enabled={enlargedText}
-                    icon={TextSizeIcon}></IconToggle>
+                    icon={TextSizeIcon}
+                    onToggle={onToggleEnlargeText}></IconToggle>
+            </div>
+            <div className='basis-1/6'>
+                <button className='bg-blue-700 rounded-md text-xs p-2'>More Details</button>
             </div>
         </div>
     );
@@ -75,7 +87,9 @@ const EmployeesPage: React.FC = () => {
                     {employeesInfo && employeesInfo.map((e) =>
                         <EmployeeCard name={e.name}
                             textToSpeechEnabled={e.textToSpeechEnabled}
-                            enlargedText={e.enlargedText}></EmployeeCard>)
+                            enlargedText={e.enlargedText}
+                            onToggleEnlargeText={() => e.enlargedText = e.enlargedText ? false : true}
+                            onToggleTextToSpeech={() => e.textToSpeechEnabled = e.textToSpeechEnabled ? false : true}></EmployeeCard>)
                     }
                 </ul>
             </div>
