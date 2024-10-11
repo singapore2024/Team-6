9d14a210df20ddcbfa162fd962828a08f6259462
gq <guanquntan@gmail.com>
2024-10-11 15:18:20 +0800

    toggle state

--- vite-project/src/pages/EmployeesPage.tsx
+++ vite-project/src/pages/EmployeesPage.tsx
@@ -18,11 +18,12 @@ const IconToggle: React.FC<IconToggleProps> = ({ enabled, icon }: IconToggleProp
         <div className="flex">
             <label className="inline-flexflex relative items-center cursor-pointer">
                 {enabled &&
-                    <input type="checkbox" value="" checked className="sr-only peer" />
+                    <input type="checkbox" value="" checked className="sr-only peer" onClick={() => enabled = !enabled} />
 
                 }
                 {!enabled &&
-                    <input type="checkbox" value="" className="sr-only peer" />
+                    <input type="checkbox" value="" className="sr-only peer"
+                        onClick={() => enabled = !enabled} />
 
                 }
                 <div className="w-9 h-5 bg-gray-200 hover:bg-gray-300 peer-focus:outline-0 peer-focus:ring-transparent rounded-full peer transition-all ease-in-out duration-500 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all peer-checked:bg-indigo-600 hover:peer-checked:bg-indigo-700"></div>
