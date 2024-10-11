e9245d9a56d13c0c171ef1b8307bd5a089589e67
jeremy chow <xelloxzxd@gmail.com>
2024-10-11 15:50:36 +0800

    fix conflict

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -7,11 +7,8 @@ import Dashboard from './Dashboard'; // Import your Dashboard component here
 import RecipeList from './Recipe';
 import Orders from './Orders';
 import Leave from './Leaves';
-<<<<<<< HEAD
 import Login from './Login';
-=======
 import EmployeesPage from './pages/EmployeesPage';
->>>>>>> 960db56d0a2159da0fe711470f0b43e2dc61dc2b
 
 function App() {
   return (
