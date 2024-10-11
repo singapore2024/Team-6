e3eef8c34c953122bd541eb6b5e6cd3f77964759
Apple <geoklinglaw@gmail.com>
2024-10-11 15:13:46 +0800

    apply leave form

--- vite-project/src/Leaves.tsx
+++ vite-project/src/Leaves.tsx
@@ -1,3 +1,4 @@
+import { colors } from "@mui/material";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -6,7 +7,7 @@ interface LeaveFormState {
   name: string;
   startDate: Date;
   endDate: Date;
-  reason: string;
+  leaveType: string;
   loading: boolean;
   formSubmitted: boolean;
 }
@@ -16,7 +17,7 @@ const ApplyLeaveForm: React.FC = () => {
     name: "",
     startDate: new Date(),
     endDate: new Date(),
-    reason: "",
+    leaveType: "",
     loading: false,
     formSubmitted: false,
   });
@@ -27,17 +28,22 @@ const ApplyLeaveForm: React.FC = () => {
     } else {
       // Optionally handle null case if the date is cleared
       setFormState({ ...formState, [fieldName]: new Date() });
+      console.log(formState)
     }
   };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+  const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = event.target;
-    setFormState({ ...formState, [name]: value });
+    setFormState(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
   };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     setFormState({ ...formState, loading: true });
+    console.log(formState)
 
     setTimeout(() => {
       setFormState({ ...formState, loading: false, formSubmitted: true });
@@ -49,39 +55,50 @@ const ApplyLeaveForm: React.FC = () => {
   }
 
   return (
-    <div className="bg-white flex flex-col justify-center items-center pb-10">
-      <h1 className="font-montserrat text-4xl mt-12 font-bold text-purple">Apply for Leave</h1>
-      <form className="flex flex-col w-[60%] mt-10" onSubmit={handleSubmit}>
-        <div>Tan Xiao Ming</div>
-        <label htmlFor="startDate" className="font-montserrat mt-5 text-lg text-grey">Start Date</label>
-        <DatePicker
-          id="startDate"
-          name="startDate"
-          className="p-3 rounded-xl bg-white text-lg"
-          selected={formState.startDate}
-          onChange={date => handleDateChange('startDate', date)}
-        />
-        <label htmlFor="endDate" className="font-montserrat mt-5 text-lg text-grey">End Date</label>
-        <DatePicker
-          id="endDate"
-          name="endDate"
-          className="p-3 rounded-xl bg-white text-lg"
-          selected={formState.endDate}
-          onChange={date => handleDateChange('endDate', date)}
-        />
-        <label htmlFor="reason" className="font-montserrat mt-5 text-lg text-grey">Reason for Leave</label>
-        <textarea
-          id="reason"
-          name="reason"
-          className="p-3 mt-1 rounded-xl bg-white text-lg h-[200px]"
-          placeholder="Enter reason for leave..."
-          maxLength={255}
-          value={formState.reason}
-          onChange={handleInputChange}
-        ></textarea>
+    <div className="bg-offwhite flex flex-col justify-center items-center">
+      <p className="font-montserrat text-[40px] mt-12 font-bold text-purple">Apply for Leave</p>
+      <form className="flex flex-col w-[90%] justify-center mt-10" onSubmit={handleSubmit}>
+            <div>
+                <p className="font-montserrat mt-5 mb-5 flex justify-start pl-3 font-thin text-grey text-[20px]">
+                    Tan Xiao Ming
+                </p>
+            </div>
+        <div className="flex flex-row">
+
+          <div className="flex flex-col w-[28%] mr-12">
+            <p className="font-montserrat mt-5 flex justify-start pl-3 font-thin text-grey">Start Date</p>
+            <DatePicker
+              name="startDate"
+              className="p-3 pr-5 rounded-xl text-black flex justify-start bg-white text-[16px]"
+              selected={formState.startDate}
+              onChange={date => handleDateChange('startDate', date)}
+            />
+          </div>
+          <div className="flex flex-col ml-10 w-[35%] ml-12">
+            <p className="font-montserrat mt-5 flex justify-start pl-3 font-thin text-grey">End Date</p>
+            <DatePicker
+              name="endDate"
+              className="p-3 pr-5 rounded-xl text-black flex justify-start bg-white text-[16px]"
+              selected={formState.endDate}
+              onChange={date => handleDateChange('endDate', date)}
+            />
+          </div>
+        </div>
+        <label className="font-montserrat mt-10 flex justify-start pl-3 font-thin text-grey">Choose Leave Type:</label>
+        <select
+            name="leaveType"
+            value={formState.leaveType}
+            onChange={handleSelectChange}
+            className="p-3 mt-1 rounded-xl bg-white text-black w-full"
+            >
+            <option value="">Select Leave type</option>
+            <option value="annual">Annual Leave</option>
+            <option value="sick">Sick Leave</option>
+            <option value="unpaid">Unpaid Leave</option>
+            </select>
         <button
           type="submit"
-          className="mt-10 bg-purple text-white py-3 rounded-lg text-lg font-bold"
+          className="bg-purple text-white py-3 rounded-lg font-bold mt-10 text-[16px] align-items font-montserrat"
           disabled={formState.loading}
         >
           {formState.loading ? 'Submitting...' : 'Apply for Leave'}
--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -4,7 +4,24 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: "#7848F4",
+        lightPurple: "#F1ECFD",
+        white: "#FFFFFF",
+        grey: "#7E7E7E",
+        darkBlue: "#080357",
+        offwhite: "#F8F8FA",
+      },
+
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat"],
+        lato: ["Lato"],
+        garamond: ["Garamond"],
+        product: ["Product Sans"],
+      },
+    },
   },
   plugins: [],
 }
